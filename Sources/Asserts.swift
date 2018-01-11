fb1b3f4caf3bf872f584576f3507949d651123cb
Ian McDowell <me@ianmcdowell.net>
2018-01-11 01:28:51 -0800

    Added thread asserts, bundle extension

---
+++ Sources/Asserts.swift
@@ -0,0 +1,19 @@
+//
+//  Asserts.swift
+//  DataUtilities
+//
+//  Created by Ian McDowell on 1/10/18.
+//  Copyright © 2018 Ian McDowell. All rights reserved.
+//
+
+import Foundation
+
+/// Assert that we are currently on the main thread
+public func assertOnMainQueue() {
+    dispatchPrecondition(condition: .onQueue(.main))
+}
+
+/// Assert that we are currently on the given thread
+public func assertOnQueue(_ queue: DispatchQueue) {
+    dispatchPrecondition(condition: .onQueue(queue))
+}
