988b4552548e05dafc24909612723c11ce1bb71b
Ian McDowell <me@ianmcdowell.net>
2018-01-16 01:03:44 -0800

    Added openssl dependency

---
+++ Sources/In-App Purchases/Receipt Validation/PurchaseReceipt.swift
@@ -0,0 +1,12 @@
+//
+//  PurchaseReceipt.swift
+//  DataUtilities
+//
+//  Created by Ian McDowell on 1/15/18.
+//  Copyright © 2018 Ian McDowell. All rights reserved.
+//
+
+import Foundation
+import ssl
+
+
